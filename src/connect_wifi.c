641244274cc36e6b427e7eba9f5bd4d74a6b260c
Yuichiro Masui <masui@masuidrive.jp>
2021-10-15 19:43:29 +0900

    added initializer for wifi

--- src/connect_wifi.c
+++ src/connect_wifi.c
@@ -70,6 +70,10 @@ esp_err_t connect_wifi(const char *ssid, const char *password) {
   strlcpy((char *)wifi_config.sta.ssid, ssid, sizeof(wifi_config.sta.ssid));
   strlcpy((char *)wifi_config.sta.password, password, sizeof(wifi_config.sta.password));
 
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
   ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
   ESP_ERROR_CHECK(esp_wifi_start());
