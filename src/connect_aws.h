3e481faf3c3b443ccbf51e2d59e584085e990575
Yuichiro Masui <masui@masuidrive.jp>
2021-10-16 14:03:46 +0900

    wip

--- src/connect_aws.h
+++ src/connect_aws.h
@@ -3,6 +3,8 @@
 #include "aws_iot_mqtt_client_interface.h"
 #include "esp_err.h"
 
-esp_err_t awsiot_connect_with_nvs();
+typedef void (*awsiot_mqtt_receiver_callback)(const char *topic, const char *payload);
+
+esp_err_t awsiot_connect_with_nvs(awsiot_mqtt_receiver_callback callback);
 esp_err_t awsiot_disconnect();
 void awsiot_loop_task(void *pvParameters);
