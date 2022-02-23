1f9769f1c27efcce831776d1265f7095f4b73385
Yuichiro Masui <masui@masuidrive.jp>
2022-02-23 12:29:58 +0900

    fixed

--- example/main/example.c
+++ example/main/example.c
@@ -20,7 +20,7 @@ struct ncb_command *extend_commands[] = {
 
 void app_main() { //
   ESP_ERROR_CHECK(esp_event_loop_create_default());
-  ncb_config_start("DeviceID", "Nordic UART", "1.0.0", "0000",extend_commands, NULL);
+  ncb_config_start("DeviceID", "Nordic UART", "1.0.0", "0000", extend_commands, NULL);
 }
 
 void command_DEBUG(int argc, const char *args[], int datac, const char *data[]) {
