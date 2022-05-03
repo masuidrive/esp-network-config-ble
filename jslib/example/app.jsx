87d5bfe3bdfac20f7d404911b3d0f1fac85a354a
Yuichiro Masui <masui@masuidrive.jp>
2022-05-03 23:44:06 +0900

    set up example env.

---
+++ jslib/example/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Component = props => {
+  return (<div>My component!</div>);
+};
+
+export const App = () => (
+  <div className="App">
+    <Component />
+  </div>
+);
---
+++ jslib/example/sw-transpiler.js
@@ -0,0 +1,22 @@
+// transpile JSX using babel so it can be run in the browser, you don't need to edit this
+// if you do edit this file, call `await window.serviceWorkerRegistration.unregister()` in the browser to update it
+
+self.addEventListener('install', e => e.waitUntil(getBabel()));
+self.addEventListener('fetch', e => e.respondWith(handleRequest(e.request)));
+
+async function getBabel() {
+  const r = await fetch('https://unpkg.com/@babel/standalone@7.17.11/babel.min.js');
+  eval(await r.text());
+}
+
+async function handleRequest(request) {
+  const url = new URL(request.url);
+  const r = await fetch(request);
+  if (r.status === 200 & url.host === location.host && !!url.pathname.match(/\.(jsx)$/)) {
+    const jsx = await r.text();
+    const js = Babel.transform(jsx, {presets: ['react']}).code;
+    return new Response(js, r);
+  } else {
+    return r;
+  }
+}
